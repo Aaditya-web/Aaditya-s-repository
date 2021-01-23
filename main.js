17d76d5b5ddc6561f565fa312cdd69fcad2fd91c
Aaditya-web <76609534+Aaditya-web@users.noreply.github.com>
2021-01-23 10:38:18 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,31 @@
-function getParagraph1()
-{
-    var inputs= []
+menu_list_array = ["Chicken Tandoori Pizza",
+"Veg Supreme Pizza",
+"Paneer Tikka Pizza",
+"Deluxe Veggie Pizza",
+"Veg Extravaganza Pizza"];
+
+function getmenu(){
+  var htmldata;
+  htmldata="<ol class='menulist'>"
+  menu_list_array.sort();
+  for(var i=0;i<menu_list_array.length;i++){
+      htmldata=htmldata+ '<li>' + menu_list_array[i] + '<li>'
     
-    for(var i = 1 ; i<=6 ; i++)
-    {
-        inputs.push(document.getElementById("para1_input_box_" + i).value);
-        inputs.join(".");
-    }
-    document.getElementById("showParagraph1").innerHTML = inputs.join(".");
+  }
+  htmldata=htmldata+"</ol>"
+  document.getElementById("display_menu").innerhtml = htmldata;
 }
+
+function add_item(){
+    var htmldata;
+    var item=document.getElementById("add_item").value;
+    menu_list_array.push(Item);
+    menu_list_array.sort();
+    htmldata="<secton class= 'cards'>"
+    for(var i=0;i<menu_list_array.length;i++){
+htmldata=htmldata+ '<div class=''card''>'+ '<img src= "images/pizzaImg.png"/>' + menu_list_array[i] + '</div>' 
+    }
+ htmldata=htmldata+"</section>"
+ document.getElementById("display_addedmenu").innerHTML = htmldata;
+}
+
