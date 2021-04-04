9cc80f3524a3b0f289bdbedff168b7e1717f8bfa
Aaditya-web <76609534+Aaditya-web@users.noreply.github.com>
2021-04-04 17:35:20 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,34 @@
+function logout()
+{
+    window.location = "index.html";
+}
+
+
+var firebaseConfig = {
+    apiKey: "AIzaSyAuSvfmsVo6v2ZSJYaG9kQ4llaVU4bSRVQ",
+    authDomain: "kwitter-eb035.firebaseapp.com",
+    databaseURL: "https://kwitter-eb035-default-rtdb.firebaseio.com",
+    projectId: "kwitter-eb035",
+    storageBucket: "kwitter-eb035.appspot.com",
+    messagingSenderId: "1053360569986",
+    appId: "1:1053360569986:web:920b97557e9e6b80903e9b",
+    measurementId: "G-JG9F2HMBPK"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  
+function getData() 
+{firebase.database().ref("/").on('value', 
+
+function(snapshot) 
+{
+document.getElementById("output").innerHTML = "";
+snapshot.forEach(function(childSnapshot) 
+{childKey  = childSnapshot.key;
+Room_names = childKey;
+//Start code
+
+
+//End code
+});});}
+getData();
