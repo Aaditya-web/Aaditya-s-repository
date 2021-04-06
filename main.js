44bc29831d891e9dbf6f50d0f827974c1700eb2a
Aaditya-web <76609534+Aaditya-web@users.noreply.github.com>
2021-04-06 10:23:32 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,60 +1,142 @@
-var last_position_of_x, last_position_of_y;
+canvas = document.getElementById('myCanvas');
+ctx = canvas.getContext("2d");
 
-    canvas = document.getElementById('myCanvas');
-    ctx = canvas.getContext("2d");
-    
-    color = "black";
-    width_of_line = 2;
+car1_width = 120;
+car1_height = 70;
+car1_image = "car1.png";
+car1_x = 10;
+car1_y = 10;
 
-    var width= screen.width;
+car2_width = 120;
+car2_height = 70;
+car2_image = "car2.png";
+car2_x = 10;
+car2_y = 10;
 
-    new_width= screen.width - 70;
-    new_height= screen.height - 300;
+background_image = "racing.jpeg";
 
-    if(width < 992)
-    {
-        document.getElementById(new_width);
-        document.getElementById(new_height);
-        document.body.style.overflow = "hidden";
-    }
+function add() {
+   background_imgTag = new Image();
+   background_imgTag.onload = uploadBackground;
+   background_imgTag.src = background_image;
 
-    canvas.addEventListener("touchstart", my_touchstart);
-    
-    function my_touchstart(e)
-    {
-        console.log("my_touchstart");
-        color = document.getElementById("color").value;
-        width_of_line = document.getElementById("width_of_line").value;
-        last_position_of_x = e.touches[0].clientX - canvas.offsetLeft;
-        last_position_of_y = e.touches[0].clientY - canvas.offsetTop;
-    }
+   car1_imgTag = new Image();
+   car1_imgTag.onload = uploadcar1;
+   car1_imgTag.src = car1_image;
 
-    canvas.addEventListener("touchmove", my_touchmove);
-    function my_touchmove(e)
-    {
-         console.log("my_touchmove");
-         current_position_of_touch_x = e.touches[0].clientX - canvas.offsetLeft;
-         current_position_of_touch_y = e.touches[0].clientY - canvas.offsetTop; 
-         
 
+   car2_imgTag = new Image();
+   car2_imgTag.onload = uploadcar2;
+   car1_imgTag.src = car1_image;
+}
+
+function uploadBackground() {
+    ctx.drawImage(background_imgTag, 0, 0, canvas_width, canvas.height)
+}
+
+function uploadcar1() {
+    ctx.drawimage(car1_imgTag, car1_x, car1_y, car1_width, car1_height);
+}
+
+window.addEventListener("keydown", my_keydown);
+
+function my_keydown(e)
 {
-        ctx.beginPath();
-        ctx.strokeStyle = color;
-        ctx.lineWidth = width_of_line;
-
-        console.log("Last position of x and y coordinates = ");
-        console.log("x = " + last_position_of_x + "y = " + last_position_of_y);
-        ctx.moveTo(last_position_of_x, last_position_of_y);
-
-        console.log("Current position of x and y coordinates = ");
-        console.log("x  = " + current_position_of_touch_x + "y = " + current_position_of_touch_y);
-        ctx.lineTo(current_position_of_touch_x, current_position_of_touch_y);
-        ctx.stroke();
+    keyPressed = e.keyCode;
+    console.log(keyPressed);
+        if(keyPressed == '38')
+        {
+            car1_up();
+            console.log("up arrow key");
+        }
+        
+        if(keyPressed == '40')
+        {
+            car1_down());
+            console.log("down arrow key");
         }
 
-        last_position_of_x = current_position_of_touch_x; 
-        last_position_of_y = current_position_of_touch_y;
-    }
-    function clearArea() 
-    { ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height); 
-    }
+        if(keyPressed == '37')
+        {
+            car1_left();
+            console.log("left arrow key");
+        }
+
+        if(keyPressed == '39')
+        {
+            car1_right();
+            console.log("right arrow key");
+        }
+
+        if(keyPressed == '87')
+        {
+            car1_up();
+            console.log("key w");
+        }
+
+        if(keyPressed == '65')
+        {
+            car1_down();
+            console.log("key a");
+        }
+
+        if(keyPressed == '83')
+        {
+            car1_left();
+            console.log("key s");
+        }
+
+        if(keyPressed == '68')
+        {
+            car1_right();
+            console.log("key d");
+        }
+
+        if(keyPressed == '38')
+        {
+            car2_up();
+            console.log("up arrow key");
+        }
+        
+        if(keyPressed == '40')
+        {
+            car2_down());
+            console.log("down arrow key");
+        }
+
+        if(keyPressed == '37')
+        {
+            car2_left();
+            console.log("left arrow key");
+        }
+
+        if(keyPressed == '39')
+        {
+            car2_right();
+            console.log("right arrow key");
+        }
+
+        if(keyPressed == '87')
+        {
+            car2_up();
+            console.log("key w");
+        }
+
+        if(keyPressed == '65')
+        {
+            car2_down();
+            console.log("key a");
+        }
+
+        if(keyPressed == '83')
+        {
+            car2_left();
+            console.log("key s");
+        }
+
+        if(keyPressed == '38')
+        {
+            car2_right();
+            console.log("key d");
+        }
+}
