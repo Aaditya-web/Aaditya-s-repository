8149bb3f21a046d5d684f7f31fdddcf038dda7f0
Aaditya-web <76609534+Aaditya-web@users.noreply.github.com>
2021-06-21 17:37:47 +0530

    Add files via upload

--- quiz_game_login.js
+++ quiz_game_login.js
@@ -22,13 +22,16 @@ function login()
 function send()
 {
     number1 = document.getElementById("number 1").value;
-    number2 = document.getElementById("number2").value;
+    number2 = document.getElementById("number 2").value;
     actual_answer = parseInt(number1) * parseInt(number2);
 
+  
     question_number = "<h4>" + number1 + " X " + number2 +"</h4>";
     input_box = "<br>Answer : <input type='text' id='input_check_box'>";
     check_button= "<br><br><button class='btn btn-info' onclick='check()'>Check</button>";
     row = question_number + input_box + check_button ;
+    document.getElementById("output").innerHTML = row;
+    document.getElementById("word").value= "";
 
     document.getElementById("output").innerHTML = row;
 
@@ -37,8 +40,8 @@ function send()
 
 }
 
-question_turn = "player1";
-answer_turn = "player2";
+question_turn = "player_1";
+answer_turn = "player_2";
 
 function check()
 {
@@ -48,13 +51,13 @@ function check()
         if(answer_turn =="player1")
         {
             update_player1_score = player1_score +1;
-            document.getElementById("player1_score").innerHTML = update_player1_score;
+            document.getElementById("player_1_score").innerHTML = update_player1_score;
 
         }
         else
         {
            update_player2_score = player2_score +1;
-           document.getElementById("player2_score").innerHTML = update_player2_score
+           document.getElementById("player_2_score").innerHTML = update_player2_score
         }
 
     }
